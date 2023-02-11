b6b647d3762feee2a62c67d12261b2c9aef1e66f
dltngurxodud <dltngurxodud@naver.com>
2023-02-11 21:05:12 +0900

    login 완료 하였습니다.

--- app.py
+++ app.py
@@ -16,6 +16,14 @@ db = client.dbsparta
 def home():
   return render_template('index.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @app.route("/diary/save", methods=["POST"])
 def diary_save():
     diary_list = list(db.anondiary.find({}))
@@ -140,6 +148,62 @@ def update_modify(param):
   print(num,file=sys.stderr)
   db.diary.update_one({'num': num}, {'$set': {'title': title,'content':content}})
   return jsonify({'msg': '수정 완료!'})
+
+# ------- 로그인
+# 회원가입 API
+@app.route('/api/register', methods=['POST'])
+def api_register():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+    nickname_receive = request.form['nickname_give']
+
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    result = db.user.find_one({'id': id_receive})
+
+    if result is not None:
+        return jsonify({'result': 'fail', 'msg': '이미 존재하는 ID입니다!'})
+    else:
+        db.user.insert_one({'id': id_receive, 'pw': pw_hash, 'nick': nickname_receive})
+        return jsonify({'result': 'success'})
+
+    return render_template('login.html')
+
+@app.route('/api/login', methods=['POST'])
+def api_login():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    result = db.user.find_one({'id': id_receive, 'pw': pw_hash})
+
+    if result is not None:
+        payload = {
+            'id': id_receive,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=100)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+        # token을 준다.
+        return jsonify({'result': 'success', 'token': token})
+        # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+@app.route('/api/nick', methods=['GET'])
+def api_valid():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        userinfo = db.user.find_one({'id': payload['id']}, {'_id': 0})
+        return jsonify({'result': 'success', 'nickname': userinfo['nick']})
+
+    except jwt.ExpiredSignatureError:
+        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
+
+    except jwt.exceptions.DecodeError:
+        return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
+#     ---------
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=4000, debug=True)
