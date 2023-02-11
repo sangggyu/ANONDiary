295204701098888effa3df6ae5f4a4ce296dd7ff
parker4942 <parkmj4312@gmail.com>
2023-02-11 16:00:15 +0900

    커밋

--- app.py
+++ app.py
@@ -7,12 +7,39 @@ from pymongo import MongoClient
 
 import certifi
 ca = certifi.where()
-client = MongoClient('mongodb+srv://test:thals@cluster0.kbk9mgh.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+# client = MongoClient('mongodb+srv://test:thals@cluster0.kbk9mgh.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+client = MongoClient('mongodb+srv://parkmj4312:Qkrp4918@cluster0.serijac.mongodb.net/?retryWrites=true&w=majority')
+
 db = client.dbsparta
 
 @app.route('/')
 def home():
   return render_template('index.html')
+
+@app.route('/diary')
+def diary():
+  return render_template('page_info.html')
+@app.route("/diary/save", methods=["POST"])
+def diary_save():
+    diary_list = list(db.anondiary.find({}))
+    numlist = [0]
+    for a in diary_list:
+        numlist.append(a['num'])
+    count = max(numlist) + 1
+    name = request.form['name_give']
+    comment = request.form['comment_give']
+    doc = {
+    'num':count,
+    'name':name,
+    'comment':comment
+    }
+    db.anondiary.insert_one(doc)
+    return jsonify({'msg': '일기 기록 완료!'})
+@app.route("/diary/show", methods=["GET"])
+def diary_get():
+    num = request.form['num_give']
+    diary = db.anondiary.find_one({'num': num})
+    return jsonify({'diary':diary})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=4000, debug=True)
